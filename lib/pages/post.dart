bca3443acfaf3ba1b38f9b0368b0559ef9e72c54
MagicianZC <18850317627@163.com>
2023-03-02 16:19:38 +0800

    抽取代码

--- lib/pages/post.dart
+++ lib/pages/post.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wechat/utils/config.dart';
 import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
 class PostEditPage extends StatefulWidget {
@@ -9,17 +10,80 @@ class PostEditPage extends StatefulWidget {
 }
 
 class _PostEditPageState extends State<PostEditPage> {
+  //已选中图片数组
+  List<AssetEntity> _selectedAssets = [];
+
+  //图片列表
+  Widget _buildPhotosList() {
+    return Padding(
+      padding: const EdgeInsets.all(spacing),
+      child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+        final double width = (constraints.maxWidth - spacing * 2) / 3;
+        return Wrap(
+          spacing: spacing,
+          runSpacing: spacing,
+          children: [
+            //图片
+            for (final asset in _selectedAssets)
+              _buildPhotoItem(asset, width),
+            //选择图片按钮
+            if (_selectedAssets.length < maxAssets)
+              _buildAddBtn(context, width),
+          ],
+        );
+      }),
+    );
+  }
+
+  //添加按钮
+  Widget _buildAddBtn(BuildContext context, double width) {
+    return GestureDetector(
+              onTap: () async {
+                final List<AssetEntity>? result =
+                    await AssetPicker.pickAssets(context,
+                        pickerConfig: AssetPickerConfig(
+                            selectedAssets: _selectedAssets,
+                            maxAssets: maxAssets));
+                setState(() {
+                  _selectedAssets = result ?? [];
+                });
+              },
+              child: Container(
+                color: Colors.black12,
+                width: width,
+                height: width,
+                child: const Icon(
+                  Icons.add,
+                  size: 48,
+                  color: Colors.black26,
+                ),
+              ),
+            );
+  }
+
+  //图片项
+  Widget _buildPhotoItem(AssetEntity asset, double width) {
+    return Container(
+              clipBehavior: Clip.antiAlias,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(3)),
+              child: AssetEntityImage(
+                asset,
+                isOriginal: false,
+                width: width,
+                height: width,
+                fit: BoxFit.cover,
+              ),
+            );
+  }
+
+  //主视图
   Widget _mainView() {
     return Column(
       children: [
-        //选取图片按钮
-        ElevatedButton(
-          onPressed: () async {
-            final List<AssetEntity>? result = await AssetPicker.pickAssets(context);
-            print(result?.length);
-          },
-          child: const Text('选取图片'),
-        ),
+        //图片列表
+        _buildPhotosList()
       ],
     );
   }
