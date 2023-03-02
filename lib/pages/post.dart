5824f4ea2e869378875b296b7212f844029aa0ca
MagicianZC <18850317627@163.com>
2023-03-02 18:37:06 +0800

    图片拖拽删除

--- lib/pages/post.dart
+++ lib/pages/post.dart
@@ -14,6 +14,12 @@ class _PostEditPageState extends State<PostEditPage> {
   //已选中图片数组
   List<AssetEntity> _selectedAssets = [];
 
+  //是否开始拖拽
+  bool isDragNow = false;
+
+  //是否将要删除
+  bool isWillRemove = false;
+
   //图片列表
   Widget _buildPhotosList() {
     return Padding(
@@ -43,7 +49,7 @@ class _PostEditPageState extends State<PostEditPage> {
         final List<AssetEntity>? result = await AssetPicker.pickAssets(context,
             pickerConfig: AssetPickerConfig(
                 selectedAssets: _selectedAssets, maxAssets: maxAssets));
-        if(result == null){
+        if (result == null) {
           return;
         }
         setState(() {
@@ -65,15 +71,31 @@ class _PostEditPageState extends State<PostEditPage> {
 
   //图片项
   Widget _buildPhotoItem(AssetEntity asset, double width) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) {
-          return GalleryWidget(
-              initialIndex: _selectedAssets.indexOf(asset),
-              items: _selectedAssets);
-        }));
+    return Draggable(
+      //拖动的数据
+      data: asset,
+      //当拖动对象开始被拖动时调用
+      onDragStarted: () {
+        setState(() {
+          isDragNow = true;
+        });
       },
-      child: Container(
+      //当拖动对象被放下时调用
+      onDragEnd: (details) {
+        setState(() {
+          isDragNow = false;
+        });
+      },
+      //当draggable被放置在DragTarget接收时调用
+      onDragCompleted: () {},
+      //当draggable被放置但未被DragTarget接收时调用
+      onDraggableCanceled: (velocity, offset) {
+        setState(() {
+          isDragNow = false;
+        });
+      },
+      //拖动进行时显示在指针下放的小部件
+      feedback: Container(
         clipBehavior: Clip.antiAlias,
         decoration: BoxDecoration(borderRadius: BorderRadius.circular(3)),
         child: AssetEntityImage(
@@ -84,6 +106,92 @@ class _PostEditPageState extends State<PostEditPage> {
           fit: BoxFit.cover,
         ),
       ),
+      //当正在进行一个或多个拖动时显示的小部件而不是child
+      childWhenDragging: Container(
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(3)),
+        child: AssetEntityImage(
+          asset,
+          isOriginal: false,
+          width: width,
+          height: width,
+          fit: BoxFit.cover,
+          opacity: const AlwaysStoppedAnimation(0.2),
+        ),
+      ),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return GalleryWidget(
+                initialIndex: _selectedAssets.indexOf(asset),
+                items: _selectedAssets);
+          }));
+        },
+        child: Container(
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(3)),
+          child: AssetEntityImage(
+            asset,
+            isOriginal: false,
+            width: width,
+            height: width,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
+  //删除Bar
+  Widget _buildRemoveBar() {
+    return DragTarget<AssetEntity>(
+      //调用以构建此小部件的内容
+      builder: (context, candidateData, rejectedData) {
+        return SizedBox(
+          width: double.infinity,
+          child: Container(
+            height: 120,
+            color: isWillRemove ? Colors.red[300] : Colors.red[200],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //图标
+                Icon(
+                  Icons.delete,
+                  size: 32,
+                  color: isWillRemove ? Colors.white:Colors.white70,
+                ),
+                //文字
+                Text(
+                  '拖拽到这里删除',
+                  style: TextStyle(
+                    color: isWillRemove ? Colors.white:Colors.white70,
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+      onWillAccept: (data) {
+        setState(() {
+          isWillRemove = true;
+        });
+        return true;
+      },
+
+      onAccept: (data) {
+        setState(() {
+          _selectedAssets.remove(data);
+          isWillRemove = false;
+        });
+      },
+
+      onLeave: (data) {
+        setState(() {
+          isWillRemove = false;
+        });
+      },
     );
   }
 
@@ -92,7 +200,7 @@ class _PostEditPageState extends State<PostEditPage> {
     return Column(
       children: [
         //图片列表
-        _buildPhotosList()
+        _buildPhotosList(),
       ],
     );
   }
@@ -104,6 +212,7 @@ class _PostEditPageState extends State<PostEditPage> {
         title: const Text('发布'),
       ),
       body: _mainView(),
+      bottomSheet: isDragNow ? _buildRemoveBar() : const SizedBox.shrink(),
     );
   }
 }
