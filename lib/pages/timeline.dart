640802975b890f55dccb544dda1f9a7ae0d4c9bf
MagicianZhang <zc930704!>
2023-07-03 00:15:04 +0800

    弹出菜单

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -6,6 +6,8 @@ import 'package:wechat/utils/index.dart';
 import 'package:wechat/widgets/index.dart';
 import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
+import '../styles/index.dart';
+
 class TimeLinePage extends StatefulWidget {
   const TimeLinePage({Key? key}) : super(key: key);
 
@@ -13,7 +15,8 @@ class TimeLinePage extends StatefulWidget {
   State<TimeLinePage> createState() => _TimeLinePageState();
 }
 
-class _TimeLinePageState extends State<TimeLinePage> {
+class _TimeLinePageState extends State<TimeLinePage>
+    with SingleTickerProviderStateMixin {
   //用户资料
   UserModel? _user;
   //动态数据
@@ -22,6 +25,19 @@ class _TimeLinePageState extends State<TimeLinePage> {
   final ScrollController _scrollController = ScrollController();
   //appbar 背景色
   Color? _appBarColor;
+
+  //曾管理
+  OverlayState? _overlayState;
+  //遮罩层
+  OverlayEntry? _shadeOverlayEntry;
+
+  //更多按钮未知offset
+  Offset _btnOffset = Offset.zero;
+
+  //动画控制器
+  late AnimationController _animationController;
+  //动画tween
+  late Animation<double> _sizeTween;
 
   //导入数据
   Future _loadData() async {
@@ -33,9 +49,33 @@ class _TimeLinePageState extends State<TimeLinePage> {
     }
   }
 
+  //获取更多按钮位置offset
+  Offset _getBtnOffset(GlobalKey key) {
+    final RenderBox renderBox =
+        key.currentContext!.findRenderObject() as RenderBox;
+    final Offset offset = renderBox?.localToGlobal(Offset.zero) ?? Offset.zero;
+    return offset;
+  }
+
   @override
   void initState() {
     super.initState();
+    //初始化overlay
+    _overlayState = Overlay.of(context);
+
+    //初始化动画控制器
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    //初始化动画tween
+    _sizeTween = Tween(begin: 0.0, end: 200.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
     //设置用户资料
     _user = UserModel(
         nickname: "聆听风雨",
@@ -49,6 +89,58 @@ class _TimeLinePageState extends State<TimeLinePage> {
     }
     //载入数据
     _loadData();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  //是否喜欢菜单
+  Widget _buildIsLikeMenu() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.black87,
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+        return Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            //喜欢
+            if (constraints.maxWidth > 80)
+              TextButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.favorite,
+                    color: Colors.redAccent,
+                    size: 16,
+                  ),
+                  label: Text(
+                    '喜欢',
+                    style: textStylePopMenu.copyWith(),
+                  )),
+            //评论
+            if (constraints.maxWidth > 150)
+              TextButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.chat_bubble_outline_rounded,
+                    color: Colors.white,
+                    size: 16,
+                  ),
+                  label: Text(
+                    '评论',
+                    style: textStylePopMenu.copyWith(),
+                  )),
+          ],
+        );
+      }),
+    );
   }
 
   //头部
@@ -123,6 +215,7 @@ class _TimeLinePageState extends State<TimeLinePage> {
   ///列表项
   Widget _buildListItem(TimelineModel item) {
     int imgCount = item.images?.length ?? 0;
+    GlobalKey btnKey = GlobalKey();
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Row(
@@ -194,14 +287,44 @@ class _TimeLinePageState extends State<TimeLinePage> {
                     color: Colors.black54,
                   ),
                 ),
-                //时间
+                //时间,更多按钮
                 const SpaceVerticalWidget(),
-                Text(
-                  item.publishDate ?? "",
-                  style: const TextStyle(
-                    fontSize: 15,
-                    color: Colors.black54,
-                  ),
+                Row(
+                  children: [
+                    //时间
+                    Text(
+                      item.publishDate ?? "",
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.black54,
+                      ),
+                    ),
+                    const SpaceHorizontalWidget(),
+                    const Spacer(),
+                    //更多按钮
+                    GestureDetector(
+                      onTap: () {
+                        //获取按钮位置
+                        var offset = _getBtnOffset(btnKey);
+                        setState(() {
+                          _btnOffset = offset;
+                        });
+                        //显示遮罩层
+                        _onShowMenu(onTap: _onCloseMenu);
+                      },
+                      child: Container(
+                        key: btnKey,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(2),
+                        ),
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        child: const Icon(
+                          Icons.more_horiz_outlined,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
                 const SpaceVerticalWidget(),
               ],
@@ -246,6 +369,55 @@ class _TimeLinePageState extends State<TimeLinePage> {
                     : PostType.image,
             selectedAssets: result);
       })));
+    }
+  }
+
+  //显示菜单
+  _onShowMenu({Function()? onTap}) {
+    _shadeOverlayEntry = OverlayEntry(builder: (context) {
+      return Positioned(
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        child: GestureDetector(
+          onTap: onTap,
+          child: Stack(children: [
+            AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                color: Colors.black.withOpacity(0.4)),
+            //菜单
+            AnimatedBuilder(
+                animation: _animationController,
+                builder: (context, child) {
+                  return Positioned(
+                      left: _btnOffset.dx - 5 - _sizeTween.value,
+                      top: _btnOffset.dy - 10,
+                      child: SizedBox(
+                          width: _sizeTween.value,
+                          height: 40,
+                          child: _buildIsLikeMenu()));
+                })
+          ]),
+        ),
+      );
+    });
+    _overlayState?.insert(_shadeOverlayEntry!);
+
+    //延迟显示菜单
+    Future.delayed(const Duration(milliseconds: 100), () {
+      if (_animationController.status == AnimationStatus.dismissed) {
+        _animationController.forward();
+      }
+    });
+  }
+
+  //关闭菜单
+  _onCloseMenu() async {
+    if (_animationController.status == AnimationStatus.completed) {
+      await _animationController.reverse();
+      _shadeOverlayEntry?.remove();
+      _shadeOverlayEntry?.dispose();
     }
   }
 
---
+++ lib/styles/text.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+///弹出菜单样式
+var textStylePopMenu = const TextStyle(
+  color: Colors.white,
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
